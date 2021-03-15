0ad07a6e53a436d7ff87cba796269063e888f099
dwachenschwanz <dwachenschwanz@smartorg.com>
2021-03-14 18:14:14 -0700

    change icon to diamond

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const icon =
   '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"></circle>';
 
+  const iconsq =
+    '<diamond cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"></diamond>';
+
 miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
       bottomBar: {
         title: "Board cleaner",
-        svgIcon: icon,
+        svgIcon: iconsq,
         onClick: async () => {
           // Show modal and wait for user choice
           let needToClear = confirm("Do you want delete all content?");
